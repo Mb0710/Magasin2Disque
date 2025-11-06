f82abd6f4b80f5f12552c5aa41465b2ebf343858
Mohamed ben <119139743+Mb0710@users.noreply.github.com>
2025-11-06 19:36:44 +0100

    Add files via upload

---
+++ src/main/java/com/saf/magasin/MagasinDisqueApplication.java
@@ -0,0 +1,12 @@
+package com.saf.magasin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagasinDisqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagasinDisqueApplication.class, args);
+    }
+}
